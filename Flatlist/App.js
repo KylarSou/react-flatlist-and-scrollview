5904bec06b35d302581e6531ac1a92b017c64baa
KylarSou <131703923+KylarSou@users.noreply.github.com>
2023-12-30 23:21:32 +0300

    Add files via upload

---
+++ Flatlist/App.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { StyleSheet, View, Text, FlatList, SafeAreaView, RefreshControl,True } from "react-native";
+import ListItem from "./src/components/ListItem";
+import ListHeader from "./src/components/ListHeader";
+import ListFooter from "./src/components/ListFooter";
+
+const DATA = [
+  {
+    id: '1',
+    title: 'First Item',
+  },
+  {
+    id: '2',
+    title: 'Second Item',
+  },
+  {
+    id: '3',
+    title: 'Third Item',
+  },
+  {
+    id: '4',
+    title: 'First Item',
+  },
+  {
+    id: '5',
+    title: 'Second Item',
+  },
+  {
+    id: '6',
+    title: 'Third Item',
+  },
+  {
+    id: '7',
+    title: 'First Item',
+  },
+  {
+    id: '8',
+    title: 'Second Item',
+  },
+  {
+    id: '9',
+    title: 'Third Item',
+  },
+  {
+    id: '10',
+    title: 'First Item',
+  },
+  {
+    id: '11',
+    title: 'Second Item',
+  },
+  {
+    id: '12',
+    title: 'Third Item',
+  },
+];
+
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+        <FlatList
+          data={DATA}
+          renderItem = {({ item }) => <ListItem item={item} />}
+          keyExtractor={(item) => item.id}
+          ListHeaderComponent={<ListHeader />}
+          ListFooterComponent={<ListFooter />}
+          refreshControl={
+            <RefreshControl refreshing={True} onRefresh={() => {}} />
+          }
+        />
+    </SafeAreaView>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
